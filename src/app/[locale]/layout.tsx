d0ada8cf9ef7b3675ca3d04c8d259d0a4dcd462c
Jackson Quintero <75138506+codigoarchivo@users.noreply.github.com>
2024-03-12 13:28:17 -0400

    refactor RootLayout

--- src/app/[locale]/layout.tsx
+++ src/app/[locale]/layout.tsx
@@ -1,13 +1,14 @@
-import type { Metadata } from "next";
-import { Inter } from "next/font/google";
-import "./globals.css";
+import type { Metadata } from 'next';
+import { Inter } from 'next/font/google';
+import './globals.css';
+import { i18n } from '../../../i18n-config';
 
-const inter = Inter({ subsets: ["latin"] });
+const inter = Inter({ subsets: ['latin'] });
 
 export const metadata: Metadata = {
   title: {
-    template: 'Atletdata - %s',
-    default: 'Atletdata',
+    template: 'Jackson Quintero - %s',
+    default: 'Jackson Quintero',
   },
 };
 
@@ -15,6 +16,11 @@ type LayoutProps = {
   params: { locale: string };
   children: React.ReactNode;
 };
+
+export function generateStaticParams() {
+  return i18n.locales.map((lang) => ({ params: { locale: lang } }));
+}
+
 export default function RootLayout({ params, children }: LayoutProps) {
   const { locale } = params;
   return (
